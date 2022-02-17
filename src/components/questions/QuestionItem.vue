075dfa150c948a53550bf901833dfddf32dd5da5
minhthangdev <thang-dm@zenkenplanet-vn.com>
2022-02-17 13:48:25 +0700

    update icon

--- src/components/questions/QuestionItem.vue
+++ src/components/questions/QuestionItem.vue
@@ -12,7 +12,7 @@
           </div>
           <high-light :text="data.label" :targets="getTargets" v-else/>
           <div class="pinned" v-if="data.isPinned" title="Result pinning">
-            <feather-icon icon="Link2Icon" size="16"/>
+            <feather-icon icon="StarIcon" size="16"/>
           </div>
       </div>
     </div>
